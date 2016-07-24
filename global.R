21ec5a18e13721153d1ec08eb15e0805d1065e0e
kaseyriver11 <kaseyriver11@gmail.com>
2016-07-23 22:22:30 -0400

    Upload II

--- global.R
+++ global.R
@@ -11,9 +11,25 @@ library(tm)
 library(rCharts)
 library(ggplot2)
 library(plyr)
+library(googleVis)
+library(DT) # For data tables;
 
 
+# Read new data and find first and last create data; 
 new1 <- new
 max <- max(new1$CreateDate)
 min <- min(new1$CreateDate)
+
+# Read the datasets from your machine;
+adjdata <- adjdata
+techdata <- techdata
+adjtechfull <- adjtechfull
+
+# List of Adjusters;
+adjtech <- c(unique(adjdata$Adjuster)) #), unique(techdata$Technician))
+
+# Selection Options;
+state <- unique(new1$State)
+type <- unique(new1$Type)
+race <- unique(new1$Race)
 
--- ui.R
+++ ui.R
@@ -8,6 +8,27 @@ navbarPage("Exploring IAA Life Insurance Data", inverse=TRUE,
          
          # Panel 1
          tabPanel("New Customers",
+                  
+          conditionalPanel(condition = "input.selection == true",
+          fluidRow(column(2,selectInput("gender", "Gender", 
+                                        choices=c("Male"="male", "Female"="female"), multiple=TRUE)),
+                   column(2,selectInput("race", "Race",
+                                        choices=c(race), multiple=TRUE)),
+                   column(2,selectInput("state", "State",
+                                        choices=c(state), multiple=TRUE)),
+                   column(2,selectInput("type", "Type of Insurance",
+                                        choices=c(type), multiple=TRUE)),
+                   column(2,selectInput("coverage", "Coverage Amount",
+                                        choices=c("$50,000"= "1", "$100,000"= "2", "$150,000" = "3"), multiple=TRUE)),
+                   column(2,selectInput("income", "Income Bracket", 
+                                        choices=c("$0-$35,000"=1,"$35,000-$70,000"=2,"$70,000+"=3), multiple=TRUE))
+                   
+          )),    
+          checkboxInput("selection", "Click to filter data"),
+                  
+                  
+                  
+                  
          fluidRow(
            column(4, align="center",   
              dateRangeInput('dateRange', 'Select Date Range:', start = min, end = max,
@@ -25,19 +46,53 @@ navbarPage("Exploring IAA Life Insurance Data", inverse=TRUE,
                                          "Type of Insurance" = "Type")))
            ),
          
+         fluidRow(column(4, textOutput("title1")),
+                  column(6, textOutput("title2"))),
+         
          
             column(4, div(showOutput("piechart", "Nvd3"), style="height:250px;"), 
                    showOutput("piechart2", "Nvd3")),
             column(8,
                 showOutput("chart2", "Nvd3"))
+         # ,
+         # fluidRow(),
+         # 
+         # fluidRow(column(8,
+         #        showOutput("line1", "Morris")))
          ),
+         
+         
          
          # Panel 2
          tabPanel("Current Customers" #,
                 #    source('external.ui/...')$value
          ),
          
-         tabPanel("Current Employees" #,
-                  #    source('external.ui/...')$value
-         )
-)))
+         
+         
+         # Panel 3
+         tabPanel("Current Adjusters",
+        
+        fluidRow(
+            column(2, align="center",   
+               selectInput('employee', 'Select Employee:', choices=c("Select Employee:" = "", as.character(adjtech)), 
+                           multiple=FALSE), selectize=TRUE),
+            column(6, htmlOutput("text1"))
+        ),
+        
+        fluidRow(
+            column(2),
+            column(8,
+            tabsetPanel(
+                tabPanel("Performance Metrics", align="left", tableOutput("empMetrics1")),
+                tabPanel("Possible Errors", align="left", tableOutput("badTable1")),
+                tabPanel("Employee Reward History", align="center", dataTableOutput("recentTable1")))
+            ))
+        )
+        )
+))
+
+
+
+
+
